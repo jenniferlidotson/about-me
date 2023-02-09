c686b881cebf98574b192f983717d884b5e8ce7b
Jennifer Li Dotson <jenniferlidotson@gmail.com>
2023-02-09 03:02:15 -0800

    added array functionality

--- js/app.js
+++ js/app.js
@@ -93,22 +93,33 @@ else if (convertNumber < 3) {
   alert(`You have ${totalAttempts} more chances to guess the correct number!`);
 }
 else (totalAttempts === 4); {
-  alert(`Shucks, no more chances left to guess.  The correct number is 3.`);
-
+  alert('Shucks, no more chances left to guess.  The correct number is 3.');
   score++;
 }
 
-let favoriteColor = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple'];
-let colorGuess = 6
+let favoriteColors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple'];
 
-while (colorGuess !++ 0){
-  let questionSeven = prompt ('What is my favorite color? You have 6 chances to guess!').toLowerCase();
+let colorGuess = 6;
 
+let correctGuess = false;
 
+while (colorGuess < 6 && correctGuess === false){
+  let questionSeven = prompt ('What are my favorite colors in the rainbow? You have 6 chances to guess!').toLowerCase();
+  console.log(questionSeven);
 
+  for(let i = 0; i < favoriteColors.length; i++){
+    if(questionSeven === favoriteColors[i]){
+      alert(`You guessed correctly, ${userName}! My favorite colors are ${favoriteColors}!`);
+      score++;
+      break;
+    }
+    if(i === favoriteColors.length - 1){
+      colorGuess++;
+      alert('Try again!');
+    }
+    if(colorGuess === 6){
+      alert(`Darn ${userName}, you didn't guess my favorite colors are ${favoriteColors}.`);
+    }
+  }
 }
-
-
-
-
 alert(`Thank you, ${userName}! I hope you enjoyed my guessing game. You correctly guessed ${score} out of 7 questions!`);
